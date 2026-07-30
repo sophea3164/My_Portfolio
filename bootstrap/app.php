de36da4cc0f08fedef90799e1e3bad05b315f44c
sophea3164 <sopheakheuy31@gmail.com>
2026-07-30 20:06:03 +0700

    Fix: Trust all proxies for Railway HTTPS reverse proxy (fixes CSS/JS asset URLs)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Trust all proxies for Railway/cloud deployment (fixes asset URLs behind HTTPS reverse proxy)
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
